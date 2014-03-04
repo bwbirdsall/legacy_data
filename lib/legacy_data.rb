7e33eb4b00d1dc30b6313632ca05220d89e5d672
Ben Birdsall & Peter Tinti <student@epicodus.com>
2014-03-04 10:06:39 -0800

    adds alphabetical sorting of result hash

--- lib/legacy_data.rb
+++ lib/legacy_data.rb
@@ -1,10 +1,20 @@
 def legacy_data(hash)
+  midpoint_array = []
+  midpoint_hash = {}
   result_hash = {}
 
   hash.each do |key, value|
     value.each do |value_item|
-      result_hash[value_item.downcase] = key
+      midpoint_hash[value_item] = key
+      midpoint_array << value_item
     end
   end
+
+  midpoint_array.sort
+
+  midpoint_array.each do |value|
+    result_hash[value.downcase] = midpoint_hash[value]
+  end
+
   result_hash
 end
